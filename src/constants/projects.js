7f15e14ab434c2cdc91c1c63f39a5500df528418
Sam Vitello <sam.j.vitello@gmail.com>
2020-12-14 08:17:16 -0700

    fix: remove kovan

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -8,11 +8,5 @@ export default [
     governorAddress: "0xe5bcea6f87aaee4a81f64dfdb4d30d400e0e5cf4",
     arbitratorAddress: "0x988b3a538b618c7a603e1c11ab82cd16dbe28069",
     snapshotSlug: 'kleros'
-  },
-  {
-    name: "kleros Kovan",
-    icon: <KlerosLogo />,
-    governorAddress: "0x5355629eebb23b4b86a623f8d247b5fe44005613",
-    arbitratorAddress: "0x60b2abfdfad9c0873242f59f2a8c32a3cc682f80",
-  },
+  }
 ];

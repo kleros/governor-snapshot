acde1fb056d3f00b47746a372125a27da8ddf739
Shaleen Jain <shaleen@jain.sh>
2021-03-26 20:10:33 +0530

    feat: add PoH governor

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ReactComponent as KlerosLogo } from "../assets/logos/kleros.svg";
+import { ReactComponent as PohLogo } from "../assets/logos/poh.svg";
 
 export default [
   {
@@ -8,5 +9,12 @@ export default [
     governorAddress: "0xe5bcea6f87aaee4a81f64dfdb4d30d400e0e5cf4",
     arbitratorAddress: "0x988b3a538b618c7a603e1c11ab82cd16dbe28069",
     snapshotSlug: 'kleros'
+  },
+  {
+    name: "Proof of Humanity",
+    icon: <PohLogo/>,
+    governorAddress: "0x327a29fcE0a6490E4236240Be176dAA282EcCfdF",
+    arbitratorAddress: "0x988b3a538b618c7a603e1c11ab82cd16dbe28069",
+    snapshotSlug: 'poh.eth'
   }
 ];
